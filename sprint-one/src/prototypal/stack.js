30f8be5a7fa949e42003efe71507f6171c5535ca
Joseph Strandmo <josephstrandmo@gmail.com>
2017-11-15 18:13:05 -0600

    Passed all tests for prototypal stack and met problem citeria

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function() { // first in first out
+  var stack = Object.create(stackMethods);
+
+  stack.front = 0;
+  stack.back = 0;
+  stack.storage = {};
+
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) { // add to back
+    this.storage[this.back] = value;
+    this.back = this.back + 1;
+  },
+  pop: function() { // remove from back
+    this.back = this.back - 1;
+    return this.storage[this.back];
+  },
+  size: function() {
+    var size = this.back - this. front;
+    if (size < 0) {
+      return 0;
+    }
+    return size;
+  }
+};
 
 
