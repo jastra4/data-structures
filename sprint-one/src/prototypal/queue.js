fb3feea2ad0e5ba723fb837427db2c4b58a19f27
Joseph Strandmo <josephstrandmo@gmail.com>
2017-11-15 18:08:41 -0600

    Passed all tests for queue prototypal and met problem criteria

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function() { // first in first out
+  //debugger;
+  var queue = Object.create(queueMethods);
+  
+  queue.front = 0;
+  queue.back = 0;
+  queue.storage = {};
+  //_.extend(queue, queueMethods);
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) { // add to back
+    this.storage[this.back] = value;
+    this.back = this.back + 1;
+  },
+  dequeue: function() { // remove from front
+    this.front = this.front + 1;
+    return this.storage[this.front - 1];
+  },
+  size: function() {
+    var size = this.back - this.front;
+    if (size < 0) {
+      return 0;
+    }
+    return size;
+  }
+};
+
+
 
 
