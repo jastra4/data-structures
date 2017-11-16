5231b10391625c8b206efecb31c3693010bcbcea
Joseph Strandmo <josephstrandmo@gmail.com>
2017-11-15 18:40:12 -0600

    Failing five tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,44 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function() { // first in first out
+  // var queue = Object.create(queueMethods) ///// prototypal
+  this.storage = {};
+  this.front = 0;
+  this.back = 0;
+
+  this.enqueue = function(value) { // add to back
+    this.storage[this.back] = value;
+    this.back = this.back + 1;
+  }
+  this.dequeue = function() { // remove from front
+    this.front = this.front + 1;
+    return this.storage[this.front - 1];
+  }
+  this.size = function() {
+  	var size = this.back - this.front;
+  	if (size < 0) {
+      return 0;
+  	}
+    return size;
+  }
+
+  return queue;
 };
 
+// var queueMethods = { // add to back
+//   enqueue: function(value) {
+//     this.storage[this.back] = value;
+//     this.back = this.back + 1;
+//   },
+//   dequeue: function() { // remove from front
+//     this.front = this.front + 1;
+//     return this.storage[this.front - 1];
+//   },
+//   size: function() {
+//     var size = this.back - this.front;
+//     if (size < 0) {
+//       return 0;
+//     }
+//     // return size; //// prototypal
+//   }
+// }
 
+var queue = new Queue(queueMethods);
