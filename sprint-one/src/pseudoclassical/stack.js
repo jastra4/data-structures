9b422d6a4131350b796c8614fb7e024a984dad9f
Joseph Strandmo <josephstrandmo@gmail.com>
2017-11-15 18:55:51 -0600

    Passed all pseudoclassical tests on the first try

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function() { // first in first out
+
+  this.storage = {};
+  this.front = 0;
+  this.back = 0;
+
 };
+
+Stack.prototype.push = function(value) { // add to back
+  this.storage[this.back] = value;
+  this.back = this.back + 1;
+}
+Stack.prototype.pop = function() { // remove from back
+  this.back = this.back - 1;
+  return this.storage[this.back];
+}
+Stack.prototype.size = function() {
+  if ((this.back - this.front) < 0) {
+  	return 0;
+  }
+  return (this.back - this.front);
+}
 
 
