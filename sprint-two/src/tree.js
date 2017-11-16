aeda73d1e2b7dc11f5deb8a6019d138e30439965
Joseph Strandmo <josephstrandmo@gmail.com>
2017-11-16 13:31:19 -0600

    Passed all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -11,21 +11,19 @@ var Tree = function(value) {
 
 var treeMethods = {
   addChild: function(value) {
-    this.children[this.children.length] = Tree(value).value;
+    this.children[this.children.length] = Tree(value);
   },
-  contains: function(target, obj) {
+  contains: function(target) {
   	debugger;
-  	var tree = obj || this;
-    if (tree.value === target) {
+    if (this.value === target) {
       return true;
-    }
-    for (var i = 0; i < tree.children.length; i++) {
-      if (tree.children[i] === target) {
-      	return true;
-      }
-      //tree.contains(target, tree.children[i]);
-    }
-
+    } 
+	for (var i = 0; i < this.children.length; i++) {
+	  var test =  this.children[i].contains(target);
+	  if (test === true) {
+	  	return true;
+	  }
+	}
     return false;
   }
 };
@@ -34,34 +32,3 @@ var treeMethods = {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-// var Tree = function(value) {
-
-//   var newTree = {};
-//   newTree.value = value;
-
-//   // your code here
-//   newTree.children = {};  // make it an array, and make addChild push newtree object into the array
-//   _.extend(newTree, treeMethods);
-//   return newTree;
-// };
-
-// var treeMethods = {
-//   addChild: function(value) {
-//   	var count = 0;
-//   	for (var key in this.children) {
-//       count++;
-//   	}
-//     this.children[count] = Tree(value).value;
-//   },
-//   contains: function(target, obj) {
-//   	tree = obj || this;
-//     if (tree.value === target) {
-//       return true;
-//     }
-//     for (var key in tree) { // change to for loop after making children array
-//       tree.contains(target, tree[key]);
-//     }
-//     return false;
-//   }
-// };
