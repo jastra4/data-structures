3c40aaeaa6ad434f177d114d02fc7cab5ffd0b3a
Joseph Strandmo <josephstrandmo@gmail.com>
2017-11-15 18:49:59 -0600

    Failing two tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -4,41 +4,22 @@ var Queue = function() { // first in first out
   this.front = 0;
   this.back = 0;
 
-  this.enqueue = function(value) { // add to back
-    this.storage[this.back] = value;
-    this.back = this.back + 1;
-  }
-  this.dequeue = function() { // remove from front
-    this.front = this.front + 1;
-    return this.storage[this.front - 1];
-  }
-  this.size = function() {
-  	var size = this.back - this.front;
-  	if (size < 0) {
-      return 0;
-  	}
-    return size;
-  }
-
-  return queue;
 };
 
-// var queueMethods = { // add to back
-//   enqueue: function(value) {
-//     this.storage[this.back] = value;
-//     this.back = this.back + 1;
-//   },
-//   dequeue: function() { // remove from front
-//     this.front = this.front + 1;
-//     return this.storage[this.front - 1];
-//   },
-//   size: function() {
-//     var size = this.back - this.front;
-//     if (size < 0) {
-//       return 0;
-//     }
-//     // return size; //// prototypal
-//   }
-// }
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.back] = value;
+  this.back = this.back + 1;
+}
+Queue.prototype.dequeue = function() {
+  this.front = this.front + 1;
+  return this.storage[this.front - 1];
+}
+Queue.prototype.size = function() {
+  var size = this.back - this.front;
+  if (size < 0) {
+    return 0;
+  }
+  return size;
+}
 
-var queue = new Queue(queueMethods);
+//var queue = new Queue(queueMethods);
