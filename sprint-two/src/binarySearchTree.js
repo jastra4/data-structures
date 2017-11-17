868901f54e6e27b9466861f2df2ab29b6402a34d
Joseph Strandmo <josephstrandmo@gmail.com>
2017-11-17 13:47:48 -0600

    added a unit test for letters and unit test for invalid values

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -4,7 +4,6 @@ var BinarySearchTree = function(value) { //functional-shared
   binaryTree.value = value;
   binaryTree.right = null; // value of greater child
   binaryTree.left = null; // value of lesser child
-  binaryTree.children = [];
   _.extend(binaryTree, binaryTreeMethods);
 
   return binaryTree;
@@ -12,30 +11,26 @@ var BinarySearchTree = function(value) { //functional-shared
 
 var binaryTreeMethods = {
   insert: function(value) {
-  	// right side
-  	if (value === 6) {
-  	}
-    if (value > this.value) {  
+  	if (value === 's') {debugger}
+    if (value > this.value) {  // check right side
       if (this.right === null) {
-        this.right = BinarySearchTree(value); // assignment 
+        this.right = BinarySearchTree(value); // add node
       } else {
       	this.right.insert(value); //recursion
       }
-    // left side
-    } else if (value < this.value) { 
+    } else if (value < this.value) { // check left side
       if (this.left === null) {
-        this.left = BinarySearchTree(value); // assignment
+        this.left = BinarySearchTree(value); // add node
       } else {
       	this.left.insert(value); //recursion
       } 
-    // neither
-    } else { 
-      console.log('value already exists');
-      return ;
+    } else if (value === this.value) { // value already exists or is invalid
+      throw new Error('Error: already exists');
+    } else {
+      return 'Error: invalid value';
     }
   },
   contains: function(value) {
-  	//debugger;
    if (value === this.value) {
      return true;
    } else if (value > this.value && this.right !== null) {
@@ -47,7 +42,6 @@ var binaryTreeMethods = {
    }
   },
   depthFirstLog: function(cb) {
-  	//debugger;
     cb(this.value);
     if (this.left !== null) {
       this.left.depthFirstLog(cb); 	
@@ -61,52 +55,3 @@ var binaryTreeMethods = {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-// var binaryTreeMethods = {
-//   insert: function(value) {
-//   	// right side
-//   	if (value === 6) {
-//   		debugger;
-//   	}
-//     if (value > this.value) {  
-//       if (this.right === null) {
-//         this.right = BinarySearchTree(value); // assignment
-//       } else if (this.right.value > value) {
-//         var temp = this.right; 
-//         this.right = BinarySearchTree(value); // assignment
-//         this.right.right = temp; 
-//       } else {
-//       	this.right.insert(value); //recursion
-//       }
-//     // left side
-//     } else if (value < this.value) { 
-//       if (this.left === null) {
-//         this.left = BinarySearchTree(value); // assignment
-//       } else if (this.left.value < value) {
-//         var temp = this.left;
-//         this.left = BinarySearchTree(value); // assignment
-//         this.left.left = temp;
-//       } else {
-//       	this.left.insert(value); //recursion
-//       } 
-//     // neither
-//     } else { 
-//       console.log('value already exists');
-//       return ;
-//     }
-//   },
-//   contains: function(value) {
-//    if (value === this.value) {
-//      return true;
-//    } else if (value > this.value) {
-//      contains(this.right);
-//    } else if (value < this.value) {
-//    	 contains(this.left) 
-//    } else {
-//    	 return false;
-//    }
-//   },
-//   depthFirstLog: function() {
-
-//   }
-// };
