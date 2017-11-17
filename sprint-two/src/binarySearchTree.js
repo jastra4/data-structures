9818f77ed21c2c6eb7648415b6737218fd6056d2
Joseph Strandmo <josephstrandmo@gmail.com>
2017-11-17 10:06:53 -0600

    Passed one test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,24 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function(value) { //functional-shared
+  var binaryTree = {};
+
+  binaryTree.right = null;
+  binaryTree.left = null;
+  _.extend(binaryTree, binaryTreeMethods);
+
+  return binaryTree;
 };
 
+var binaryTreeMethods = {
+  insert: function() {
+
+  },
+  contains: function() {
+
+  },
+  depthFirstLog: function() {
+
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
