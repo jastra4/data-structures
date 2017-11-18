20ec3bdce73660f12e4d1fb6ae405bb42f51ec57
Joseph Strandmo <josephstrandmo@gmail.com>
2017-11-18 12:00:19 -0600

    Passed test for resizing hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,27 +1,23 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this.storageTracker = []; // tracks new indexes
+  this.resizeTracker = []; // tracks new indexes
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(index) === undefined) {
     this._storage.set(index, []); // set hash index to mapArray
-    this.storageTracker.push(index); // keep track of new indexes
   }
   this._storage.get(index).unshift([k]);    
   this._storage.get(index)[k] = v;
-  if (this.storageTracker.length >= (.75 * this._limit)) {
+  this.resizeTracker.push(index);
+  if (this.resizeTracker.length > (.75 * this._limit)) {
     this.double(this); // resizes hashTable
   }
-  // if (this._storage.get(index).length >= (.75 * this._limit)) { // check bucket length
-  //   this.double(this);
-  // }
 };
 
 HashTable.prototype.retrieve = function(k) {
-  //debugger;
   var index = getIndexBelowMaxForKey(k, this._limit);
   return this._storage.get(index)[k];
 };
@@ -31,26 +27,38 @@ HashTable.prototype.remove = function(k) {
   this._storage.set(index, [] )
 };
 
-HashTable.prototype.double = function(oldHashTable) {
-  //console.log('double ran');
-  var newHashTable = new HashTable();
-  newHashTable._limit = (oldHashTable._limit * 2);
+HashTable.prototype.double = function(hashTable) {
+  var tempTable = hashTable;
+  var allTuples = [];
 
-  // iterate over oldHashTable
-  oldHashTable._storage.each(function(element, index, coll) { //_storage = hashArray, element = mapArray
-    if (element !== undefined) {
-        debugger;
-    //console.log(oldHashTable.retrieve('Steven1'));
-      for (var i = 0; i < element.length; i++) {
-        console.log(element[i][0]);
-        console.log(oldHashTable.retrieve(element[i][0]));
-        newHashTable.insert(element[i][0], oldHashTable.retrieve(element[i][0]));
+  // stores old tuples
+  tempTable._storage.each(function(bucket, index) { //_storage = hashArray, element = mapArray
+    if (bucket !== undefined) {
+      for (var i = 0; i < bucket.length; i++) {
+        var tupleArray = [];
+        tupleArray.push(bucket[i][0]);
+        tupleArray.push(tempTable.retrieve(bucket[i][0]));
+        allTuples.push(tupleArray);
       }
     }
   }); 
 
-  hashTable = newHashTable;
-  return hashTable;
+  // modifies hashTable
+  hashTable._limit = (tempTable._limit * 2);
+  hashTable._storage = LimitedArray(hashTable._limit);
+  hashTable.resizeTracker = [];
+
+  // wipes old tuples
+  hashTable._storage.each(function(bucket, index, hashTable) {
+    if (bucket !== undefined) {
+      bucket = [];
+    }
+  });
+  
+  // repopulates hashTable
+  for (var i = 0; i < allTuples.length; i++) {
+    hashTable.insert(allTuples[i][0], allTuples[i][1]);
+  }
 };
 
 /*
