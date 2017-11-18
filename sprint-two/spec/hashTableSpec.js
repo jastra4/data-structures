58ec4e8fe834bc0d4b871b6d2281d44428ba8249
Joseph Strandmo <josephstrandmo@gmail.com>
2017-11-18 10:34:16 -0600

    some progress resizing hashTable

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -7,45 +7,57 @@ describe('hashTable', function() {
     hashTable = new HashTable();
   });
 
-  it('should have methods named "insert", "remove", and "retrieve', function() {
-    expect(hashTable.insert).to.be.a('function');
-    expect(hashTable.remove).to.be.a('function');
-    expect(hashTable.retrieve).to.be.a('function');
+  // it('should have methods named "insert", "remove", and "retrieve', function() {
+  //   expect(hashTable.insert).to.be.a('function');
+  //   expect(hashTable.remove).to.be.a('function');
+  //   expect(hashTable.retrieve).to.be.a('function');
+  // });
+
+  // it('should store values that were inserted', function() {
+  //   hashTable.insert('Steven', 'Seagal');
+  //   expect(hashTable.retrieve('Steven')).to.equal('Seagal');
+  // });
+
+  // it('should not contain values that were not inserted', function() {
+  //   hashTable.insert('Steven', 'Spielberg');
+  //   expect(hashTable.retrieve('Steven')).not.to.equal('Seagal');
+  // });
+
+  // it('should overwrite values that have the same key', function() {
+  //   hashTable.insert('Bob', 'Loblaw');
+  //   hashTable.insert('Bob', 'Barker');
+  //   expect(hashTable.retrieve('Bob')).to.equal('Barker');
+  // });
+
+  // it('should not contain values that were removed', function() {
+  //   hashTable.insert('Steven', 'Tyler');
+  //   hashTable.remove('Steven');
+  //   expect(hashTable.retrieve('Steven')).to.equal(undefined);
+  // });
+
+  // it('should handle hash function collisions', function() {
+  //   var v1 = 'val1';
+  //   var v2 = 'val2';
+  //   var oldHashFunction = window.getIndexBelowMaxForKey;
+  //   window.getIndexBelowMaxForKey = function() { return 0; };
+  //   hashTable.insert(v1, v1);
+  //   hashTable.insert(v2, v2);
+  //   expect(hashTable.retrieve(v1)).to.equal(v1);
+  //   expect(hashTable.retrieve(v2)).to.equal(v2);
+  //   window.getIndexBelowMaxForKey = oldHashFunction;
+  // });
+
+  it('should know when to double in size', function() {
+    for (var i = 0; i < 100; i++) {
+    hashTable.insert('Steven'+[i], 'Tyler'+[i]);
+    hashTable.insert('Steven1'+[i], 'Tyler1'+[i]);
+    hashTable.insert('Steven2'+[i], 'Tyler2'+[i]);
+    hashTable.insert('Steven3'+[i], 'Tyler3'+[i]);
+    hashTable.insert('Steven4'+[i], 'Tyler4'+[i]);
+    }               
+    expect(hashTable._limit).to.equal(16);
   });
 
-  it('should store values that were inserted', function() {
-    hashTable.insert('Steven', 'Seagal');
-    expect(hashTable.retrieve('Steven')).to.equal('Seagal');
-  });
-
-  it('should not contain values that were not inserted', function() {
-    hashTable.insert('Steven', 'Spielberg');
-    expect(hashTable.retrieve('Steven')).not.to.equal('Seagal');
-  });
-
-  it('should overwrite values that have the same key', function() {
-    hashTable.insert('Bob', 'Loblaw');
-    hashTable.insert('Bob', 'Barker');
-    expect(hashTable.retrieve('Bob')).to.equal('Barker');
-  });
-
-  it('should not contain values that were removed', function() {
-    hashTable.insert('Steven', 'Tyler');
-    hashTable.remove('Steven');
-    expect(hashTable.retrieve('Steven')).to.equal(undefined);
-  });
-
-  it('should handle hash function collisions', function() {
-    var v1 = 'val1';
-    var v2 = 'val2';
-    var oldHashFunction = window.getIndexBelowMaxForKey;
-    window.getIndexBelowMaxForKey = function() { return 0; };
-    hashTable.insert(v1, v1);
-    hashTable.insert(v2, v2);
-    expect(hashTable.retrieve(v1)).to.equal(v1);
-    expect(hashTable.retrieve(v2)).to.equal(v2);
-    window.getIndexBelowMaxForKey = oldHashFunction;
-  });
 
   // (Advanced! Remove the extra "x" when you want the following tests to run)
   xit ('should double in size when needed', function() {
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,22 +1,27 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.storageTracker = []; // tracks new indexes
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(index) === undefined) {
-    this._storage.set(index, []);
-    this._storage.get(index).unshift([k]);    
-    this._storage.get(index)[k] = v; 
+    this._storage.set(index, []); // set hash index to mapArray
+    this.storageTracker.push(index); // keep track of new indexes
   }
   this._storage.get(index).unshift([k]);    
-  this._storage.get(index)[k] = v; 
+  this._storage.get(index)[k] = v;
+  if (this.storageTracker.length >= (.75 * this._limit)) {
+    this.double(this); // resizes hashTable
+  }
+  // if (this._storage.get(index).length >= (.75 * this._limit)) { // check bucket length
+  //   this.double(this);
+  // }
 };
 
 HashTable.prototype.retrieve = function(k) {
+  //debugger;
   var index = getIndexBelowMaxForKey(k, this._limit);
   return this._storage.get(index)[k];
 };
@@ -26,9 +31,28 @@ HashTable.prototype.remove = function(k) {
   this._storage.set(index, [] )
 };
 
+HashTable.prototype.double = function(oldHashTable) {
+  //console.log('double ran');
+  var newHashTable = new HashTable();
+  newHashTable._limit = (oldHashTable._limit * 2);
 
+  // iterate over oldHashTable
+  oldHashTable._storage.each(function(element, index, coll) { //_storage = hashArray, element = mapArray
+    if (element !== undefined) {
+        debugger;
+    //console.log(oldHashTable.retrieve('Steven1'));
+      for (var i = 0; i < element.length; i++) {
+        console.log(element[i][0]);
+        console.log(oldHashTable.retrieve(element[i][0]));
+        newHashTable.insert(element[i][0], oldHashTable.retrieve(element[i][0]));
+      }
+    }
+  }); 
+
+  hashTable = newHashTable;
+  return hashTable;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
